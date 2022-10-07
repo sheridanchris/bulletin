f56fd4d05bc81a1e24e3352d388ca735632fba26
Cody <cody@codyq.dev>
2022-10-07 17:31:34 -0500

    ihi

--- src/Bulletin/Program.fs
+++ src/Bulletin/Program.fs
@@ -1,10 +1,41 @@
-﻿open Falco
+﻿open System
+open Falco
 open Falco.Routing
 open Falco.HostBuilder
 
-let add x y = x + y
+type User = { Id: int; Username: string }
 
-let addFive = add 5
+type Poster =
+    | User of int
+    | Robot
+
+type Post =
+    { Id: int
+      Headline: string
+      Link: string
+      Poster: Poster
+      CreatedAt: DateTime
+      UpdatedAt: DateTime }
+
+type VoteType =
+    | Positive
+    | Negative
+
+type Vote =
+    { PostId: int
+      UserId: int
+      Type: VoteType }
+
+type Comment =
+    { Id: Guid
+      PostId: int
+      UserId: int
+      Parent: Guid option }
+
+type CommentVote =
+    { CommentId: Guid
+      UserId: int
+      Type: VoteType }
 
 [<EntryPoint>]
 let main args =
