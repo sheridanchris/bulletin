5822898bd91a402ec474d774f6576c5335af185f
Christian Sheridan <christiansheridan@outlook.com>
2022-10-07 18:56:18 -0500

    Add google oauth

--- src/Bulletin/Program.fs
+++ src/Bulletin/Program.fs
@@ -1,9 +1,34 @@
-﻿open System
+﻿open Microsoft.Extensions.DependencyInjection
+open Microsoft.AspNetCore.Authentication.Google
 open Falco
 open Falco.Routing
 open Falco.HostBuilder
 
-[<EntryPoint>]
+let configuration = configuration [||] {
+    optional_json "appsettings.Development.json"
+    required_json "appsettings.json"
+}
+
+let googleOptions: GoogleOptions -> unit =
+    fun googleOptions ->
+        googleOptions.ClientId <- configuration["Google.ClientId"]
+        googleOptions.ClientSecret <- configuration["Google.Secret"]
+
+let configureServices (serviceCollection: IServiceCollection) =
+    serviceCollection.AddAuthentication().AddGoogle(googleOptions) |> ignore
+    serviceCollection
+
+[<EntryPoint>] 
 let main args =
-    webHost args { endpoints [ get "/ping" (Response.ofPlainText "pong") ] }
+    webHost args {
+        add_service configureServices
+        use_authentication
+        use_authorization
+        use_compression
+        use_caching
+
+        endpoints [ 
+            get "/ping" (Response.ofPlainText "pong")
+        ]
+    }
     0
