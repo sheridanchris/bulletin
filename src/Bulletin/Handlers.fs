dd4d09ec3f858dd64e28491a0651a4359ab24162
Christian Sheridan <christiansheridan@outlook.com>
2022-10-12 22:21:45 -0500

    Improve vote type logic

--- src/Bulletin/Handlers.fs
+++ src/Bulletin/Handlers.fs
@@ -51,16 +51,15 @@ let postsHandler: HttpHandler =
         let score = votes |> getScore
 
         let upvoted, downvoted =
-            match currentUserId with
-            | None -> false, false
-            | Some userId ->
-                match votes |> List.tryFind (fun vote -> vote.VoterId = userId) with
-                | None -> false, false
-                | Some vote -> 
-                    match vote.Type with
-                    | VoteType.Positive -> true, false
-                    | VoteType.Negative -> false, true
-                    | _ -> false, false
+            let postVoteType =
+                currentUserId
+                |> Option.bind (fun userId -> votes |> List.tryFind (fun vote -> vote.VoterId = userId))
+                |> Option.map (fun postVote -> postVote.Type)
+
+            match postVoteType with
+            | Some VoteType.Positive -> true, false
+            | Some VoteType.Negative -> false, true
+            | Some _ | None -> false, false  
 
         {| headline = post.Headline
            score = score
@@ -69,10 +68,7 @@ let postsHandler: HttpHandler =
            downvoted = downvoted |}
 
     let postModels currentUserId posts =
-        posts
-        |> Seq.toList
-        |> List.groupBy fst3
-        |> List.map (toModel currentUserId)
+        posts |> Seq.toList |> List.groupBy fst3 |> List.map (toModel currentUserId)
 
     let handler (dbConnectionFactory: DbConnectionFactory) : HttpHandler =
         fun ctx ->
