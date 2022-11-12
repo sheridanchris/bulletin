d5c99b8fdb03e1709a0973371377ca2b646de11d
Tyler <hockey302002@gmail.com>
2022-11-12 17:31:40 -0600

    New user interface

--- src/Bulletin/wwwroot/script.js
+++ src/Bulletin/wwwroot/script.js
@@ -5,3 +5,16 @@ function setPage(page) {
         window.location.search = searchParams.toString();
     }
 }
+
+const showReplies = document.querySelectorAll('.commentReplies')
+
+showReplies.forEach(btn => btn.addEventListener('click', (e) => {
+    let parentContainer = e.target.closest('.comment-container')
+    let _id = parentContainer.id
+
+    if(_id) {
+        let childrenContainer = parentContainer.querySelectorAll(`[dataset=${_id}]`)
+
+        childrenContainer.forEach(child => child.classList.toggle('opened'))
+    }
+}))
