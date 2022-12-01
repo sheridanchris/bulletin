314a87c29173e23ea887da5c7a9f1a9a0653a37d
Christian Sheridan <christiansheridan@outlook.com>
2022-11-30 20:31:48 -0600

    Update pagination buttons

--- src/Client/Pages/FeedPage.fs
+++ src/Client/Pages/FeedPage.fs
@@ -68,7 +68,7 @@ let Pagination
   let nextButton () =
     html
       $"""
-      <button @click={Ev(fun _ -> goToNext ())} class="px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</button>
+      <button @click={Ev(fun _ -> goToNext ())} class="px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</button>
       """
 
   html
