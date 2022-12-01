b05e8486df460deb576b87a70d7a1503b710ceb8
Christian Sheridan <christiansheridan@outlook.com>
2022-11-30 20:24:58 -0600

    Cache app state in application context

--- src/Client/Pages/FeedPage.fs
+++ src/Client/Pages/FeedPage.fs
@@ -3,90 +3,8 @@ module FeedPage
 open System
 open Lit
 open Lit.Elmish
+open LitStore
 open Shared
-
-type State = {
-  Model: GetFeedRequest
-  Posts: Paginated<PostModel>
-  SubscribedFeeds: SubscribedFeed list
-}
-
-type Msg =
-  | Search
-  | SetPage of int
-  | SetSearchQuery of string
-  | SetOrdering of string
-  | SetPosts of Paginated<PostModel>
-  | SetSubscribedFeeds of SubscribedFeed list
-  | SetSelectedFeed of string
-
-let getUserFeedFromServerCmd request =
-  Elmish.Cmd.OfAsync.perform Remoting.securedServerApi.GetUserFeed request SetPosts
-
-let init () =
-  let getPostsModel = {
-    Ordering = Newest
-    SearchQuery = None
-    Feed = None
-    Page = 1
-    PageSize = 25
-  }
-
-  {
-    Model = getPostsModel
-    Posts = Paginated.empty ()
-    SubscribedFeeds = []
-  },
-  Elmish.Cmd.batch [
-    getUserFeedFromServerCmd getPostsModel
-    Elmish.Cmd.OfAsync.perform Remoting.securedServerApi.GetSubscribedFeeds () SetSubscribedFeeds
-  ]
-
-let update (msg: Msg) (state: State) =
-  match msg with
-  | SetPosts posts -> { state with Posts = posts }, Elmish.Cmd.none
-  | SetSubscribedFeeds feeds -> { state with SubscribedFeeds = feeds }, Elmish.Cmd.none
-  | Search ->
-    let model = { state.Model with Page = 1 }
-    { state with Model = model }, getUserFeedFromServerCmd state.Model
-  | SetPage page ->
-    if page < 0 || page > state.Posts.PageCount then
-      state, Elmish.Cmd.none
-    else
-      let model = { state.Model with Page = page }
-      { state with Model = model }, getUserFeedFromServerCmd model
-  | SetSearchQuery query ->
-    let query = if String.IsNullOrWhiteSpace query then None else Some query
-    let model = { state.Model with SearchQuery = query }
-    { state with Model = model }, Elmish.Cmd.none
-  | SetOrdering ordering ->
-    let ordering =
-      match ordering with
-      | ordering when ordering = string Ordering.Updated -> Ordering.Updated
-      | ordering when ordering = string Ordering.Oldest -> Ordering.Oldest
-      | ordering when ordering = string Ordering.Newest -> Ordering.Newest
-      | _ -> Ordering.Updated
-
-    let model =
-      { state.Model with
-          Ordering = ordering
-          Page = 1
-      }
-
-    { state with Model = model }, getUserFeedFromServerCmd model
-  | SetSelectedFeed feedValue ->
-    let feedId =
-      match Guid.TryParse(feedValue) with
-      | true, value -> Some value
-      | false, _ -> None
-
-    let model =
-      { state.Model with
-          Feed = feedId
-          Page = 1
-      }
-
-    { state with Model = model }, getUserFeedFromServerCmd model
 
 [<HookComponent>]
 let Post (post: PostModel) =
@@ -173,7 +91,7 @@ let Pagination
     """
 
 [<HookComponent>]
-let FeedSelector (selectedFeed: Guid option) (feeds: SubscribedFeed list) (dispatch: Msg -> unit) =
+let FeedSelector (selectedFeed: Guid option) (feeds: SubscribedFeed list) (dispatch: ApplicationContext.Msg -> unit) =
   let option (feed: SubscribedFeed) =
     html
       $"""
@@ -182,7 +100,7 @@ let FeedSelector (selectedFeed: Guid option) (feeds: SubscribedFeed list) (dispa
 
   html
     $"""
-    <select @change={EvVal(SetSelectedFeed >> dispatch)} id="feed" class="w-full sm:w-1/12 block px-4 py-3 text-base text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+    <select @change={EvVal(ApplicationContext.SetSelectedFeed >> dispatch)} id="feed" class="w-full sm:w-1/12 block px-4 py-3 text-base text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
       <option ?selected={selectedFeed = None} value="none">All Feeds</option>
       {feeds |> List.map option}
     </select>
@@ -190,7 +108,7 @@ let FeedSelector (selectedFeed: Guid option) (feeds: SubscribedFeed list) (dispa
 
 [<HookComponent>]
 let Component () =
-  let state, dispatch = Hook.useElmish (init, update)
+  let store = Hook.useStore ApplicationContext.store
 
   let displayPosts posts =
     html
@@ -204,10 +122,7 @@ let Component () =
       """
 
   let displayEmptyFeed () =
-    html
-      $"""
-      <h1>Sorry, there's nothing in your feed :(</h1>
-      """
+    html $"<h1>Sorry, there's nothing in your feed :(</h1>"
 
   html
     $"""
@@ -219,28 +134,28 @@ let Component () =
                 <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                     <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                 </div>
-                <input @change={EvVal(SetSearchQuery >> dispatch)} type="search" id="search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required>
-                <button @click={Ev(fun _ -> dispatch Search)} type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                <input @change={EvVal(ApplicationContext.SetSearchQuery >> ApplicationContext.dispatch)} type="search" id="search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required>
+                <button @click={Ev(fun _ -> ApplicationContext.dispatch ApplicationContext.Search)} type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
             </div>
         </div>
-        <select @change={EvVal(SetOrdering >> dispatch)} id="ordering" class="w-full sm:w-1/12 block px-4 py-3 text-base text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-          <option ?selected={state.Model.Ordering = Ordering.Newest}>{string Ordering.Newest}</option>
-          <option ?selected={state.Model.Ordering = Ordering.Updated}>{string Ordering.Updated}</option>
-          <option ?selected={state.Model.Ordering = Ordering.Oldest}>{string Ordering.Oldest}</option>
+        <select @change={EvVal(ApplicationContext.SetOrdering >> ApplicationContext.dispatch)} id="ordering" class="w-full sm:w-1/12 block px-4 py-3 text-base text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+          <option ?selected={store.GetFeedRequest.Ordering = Ordering.Newest}>{string Ordering.Newest}</option>
+          <option ?selected={store.GetFeedRequest.Ordering = Ordering.Updated}>{string Ordering.Updated}</option>
+          <option ?selected={store.GetFeedRequest.Ordering = Ordering.Oldest}>{string Ordering.Oldest}</option>
         </select>
-        {FeedSelector state.Model.Feed state.SubscribedFeeds dispatch}
+        {FeedSelector store.GetFeedRequest.Feed store.SubscribedFeeds ApplicationContext.dispatch}
       </div>
-      {if List.length state.Posts.Items > 0 then
-         displayPosts state.Posts.Items
+      {if List.length store.Posts.Items > 0 then
+         displayPosts store.Posts.Items
        else
          displayEmptyFeed ()}
       {Pagination
-         state.Posts.CurrentPage
-         state.Posts.PageCount
-         state.Posts.HasPreviousPage
-         state.Posts.HasNextPage
-         (fun _ -> dispatch (SetPage(state.Posts.CurrentPage - 1)))
-         (fun _ -> dispatch (SetPage(state.Posts.CurrentPage + 1)))
-         (SetPage >> dispatch)}
+         store.Posts.CurrentPage
+         store.Posts.PageCount
+         store.Posts.HasPreviousPage
+         store.Posts.HasNextPage
+         (fun _ -> ApplicationContext.dispatch (ApplicationContext.SetPage(store.Posts.CurrentPage - 1)))
+         (fun _ -> ApplicationContext.dispatch (ApplicationContext.SetPage(store.Posts.CurrentPage + 1)))
+         (ApplicationContext.SetPage >> ApplicationContext.dispatch)}
     </div>
     """
