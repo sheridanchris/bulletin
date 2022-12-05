1e3695616e5ab4e25ab467dac74daa16e89706c5
Christian Sheridan <christiansheridan@outlook.com>
2022-12-05 07:51:21 -0600

    Inputs now contain relevant profile information

--- src/Client/Pages/EditProfilePage.fs
+++ src/Client/Pages/EditProfilePage.fs
@@ -86,10 +86,7 @@ let update (msg: Msg) (state: State) =
     let alert = Danger { Reason = "Failed to edit your profile." }
     { state with Alert = alert }, Elmish.Cmd.none
 
-[<HookComponent>]
-let Component () =
-  let state, dispatch = Hook.useElmish (init, update)
-
+let renderUser (state: State) (dispatch: Msg -> unit) (user: UserModel) =
   let optionalErrorComponent (key: string) (validationState: ValidationState<'a> option) =
     match validationState with
     | None -> Lit.nothing
@@ -104,17 +101,17 @@ let Component () =
           <h5 class="text-xl font-medium text-gray-900 dark:text-white">Edit your profile</h5>
           <div>
             <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your username</label>
-            <input @change={EvVal(SetUsername >> dispatch)} type="text" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="username">
+            <input .value={user.Username} @change={EvVal(SetUsername >> dispatch)} type="text" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="username">
             {optionalErrorComponent "Username" state.Username}
           </div>
           <div>
             <label for="email-address" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email address</label>
-            <input @change={EvVal(SetEmailAddress >> dispatch)} type="text" name="email-address" id="email-address" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="email address">
+            <input .value={user.EmailAddress} @change={EvVal(SetEmailAddress >> dispatch)} type="text" name="email-address" id="email-address" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="email address">
             {optionalErrorComponent "Email address" state.EmailAddress}
           </div>
           <div>
             <label for="gravatar-email-address" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your gravatar email address</label>
-            <input @change={EvVal(SetGravatarEmailAddress >> dispatch)} type="text" name="gravatar-email-address" id="gravatar-email-address" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="gravatar email address">
+            <input .value={user.GravatarEmailAddress} @change={EvVal(SetGravatarEmailAddress >> dispatch)} type="text" name="gravatar-email-address" id="gravatar-email-address" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="gravatar email address">
             {optionalErrorComponent "Gravatar email address" state.GravatarEmailAddress}
           </div>
           <button @click={Ev(fun _ -> dispatch Submit)} class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Edit profile</button>
@@ -122,3 +119,12 @@ let Component () =
       </div>
     </div>
     """
+
+[<HookComponent>]
+let Component () =
+  let state, dispatch = Hook.useElmish (init, update)
+  let store = Hook.useStore ApplicationContext.store
+
+  match store.User with
+  | Anonymous -> Lit.nothing
+  | User user -> renderUser state dispatch user
