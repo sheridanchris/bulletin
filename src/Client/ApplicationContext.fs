9356b3c5adf29b7fd3e3384226a446228fae6543
Christian Sheridan <christiansheridan@outlook.com>
2022-12-31 12:28:42 -0600

    Create functions to reduce size of match expr

--- src/Client/ApplicationContext.fs
+++ src/Client/ApplicationContext.fs
@@ -42,84 +42,103 @@ let init () =
   },
   Cmd.OfAsync.perform Remoting.unsecuredServerApi.GetCurrentUser () SetCurrentUser
 
-let private getUserFeedFromServerCmd request =
-  Cmd.OfAsync.perform Remoting.securedServerApi.GetUserFeed request SetPosts
+let private getUserFeedCmd model =
+  Cmd.OfAsync.perform Remoting.securedServerApi.GetUserFeed model.GetFeedRequest SetPosts
+
+let private setUser user model =
+  match user with
+  | User user ->
+    let model = { model with User = User user }
+
+    model,
+    Cmd.batch [
+      Cmd.OfAsync.perform Remoting.securedServerApi.GetSubscribedFeeds () SetSubscribedFeeds
+      getUserFeedCmd model
+    ]
+  | Anonymous ->
+    { model with
+        User = Anonymous
+        SubscribedFeeds = []
+        Posts = Paginated.empty ()
+    },
+    Cmd.none
+
+let private setOrdering ordering model =
+  let ordering =
+    match ordering with
+    | ordering when ordering = string Oldest -> Oldest
+    | ordering when ordering = string Newest -> Newest
+    | ordering when ordering = string Updated -> Updated
+    | _ -> Updated
+
+  let getFeedRequest =
+    { model.GetFeedRequest with
+        Ordering = ordering
+        Page = 1
+    }
+
+  let model = { model with GetFeedRequest = getFeedRequest }
+  model, getUserFeedCmd model
+
+let deleteFeed feedId model =
+  let subscribedFeed =
+    model.SubscribedFeeds
+    |> List.indexed
+    |> List.tryFind (fun (_, subscription) -> subscription.Id = feedId)
+
+  match subscribedFeed with
+  | None -> model, Cmd.none
+  | Some(index, _) ->
+    // TODO: not sure if I should update the feed here, or elsewhere.
+    let subscribedFeeds = List.removeAt index model.SubscribedFeeds
+    let model = { model with SubscribedFeeds = subscribedFeeds }
+    model, getUserFeedCmd model
+
+let private search model =
+  let getFeedRequest = { model.GetFeedRequest with Page = 1 }
+  let model = { model with GetFeedRequest = getFeedRequest }
+  model, getUserFeedCmd model
+
+let private setSearchQuery query model =
+  let query = if String.IsNullOrWhiteSpace query then None else Some query
+  let getFeedRequest = { model.GetFeedRequest with SearchQuery = query }
+  { model with GetFeedRequest = getFeedRequest }, Cmd.none
+
+let private setPage page model =
+  match page with
+  | page when page < 0 || page > model.Posts.PageCount -> model, Cmd.none
+  | page ->
+    let getFeedRequest = { model.GetFeedRequest with Page = page }
+    let model = { model with GetFeedRequest = getFeedRequest }
+    model, getUserFeedCmd model
+
+let private setSelectedFeed (feedValueString: string) model =
+  let feedId =
+    match Guid.TryParse(feedValueString) with
+    | true, value -> Some value
+    | false, _ -> None
+
+  let getFeedRequest =
+    { model.GetFeedRequest with
+        Feed = feedId
+        Page = 1
+    }
+
+  let model = { model with GetFeedRequest = getFeedRequest }
+  model, getUserFeedCmd model
 
 let update (msg: Msg) (model: Model) =
   match msg with
-  | SetCurrentUser user ->
-    match user with
-    | User user ->
-      { model with User = User user },
-      Cmd.batch [
-        Cmd.OfAsync.perform Remoting.securedServerApi.GetSubscribedFeeds () SetSubscribedFeeds
-        getUserFeedFromServerCmd model.GetFeedRequest
-      ]
-    | Anonymous ->
-      { model with
-          User = Anonymous
-          SubscribedFeeds = []
-          Posts = Paginated.empty ()
-      },
-      Cmd.none
-  | SetSubscribedFeeds feeds -> { model with SubscribedFeeds = feeds }, Cmd.none
-  | AddFeedToContext feed ->
-    // TODO: not sure if I should update the feed here, or elsewhere.
-    { model with SubscribedFeeds = feed :: model.SubscribedFeeds }, getUserFeedFromServerCmd model.GetFeedRequest
-  | DeleteFeedFromContext feedId ->
-    let subscribedFeed =
-      model.SubscribedFeeds
-      |> List.indexed
-      |> List.tryFind (fun (_, subscription) -> subscription.Id = feedId)
-
-    match subscribedFeed with
-    | Some(index, _) ->
-      // TODO: not sure if I should update the feed here, or elsewhere.
-      { model with SubscribedFeeds = List.removeAt index model.SubscribedFeeds },
-      getUserFeedFromServerCmd model.GetFeedRequest
-    | None -> model, Cmd.none
-  | Search ->
-    let getFeedRequest = { model.GetFeedRequest with Page = 1 }
-    { model with GetFeedRequest = getFeedRequest }, getUserFeedFromServerCmd getFeedRequest
-  | SetPage page ->
-    if page < 0 || page > model.Posts.PageCount then
-      model, Cmd.none
-    else
-      let getFeedRequest = { model.GetFeedRequest with Page = page }
-      { model with GetFeedRequest = getFeedRequest }, getUserFeedFromServerCmd getFeedRequest
-  | SetSearchQuery query ->
-    let query = if String.IsNullOrWhiteSpace query then None else Some query
-    let getFeedRequest = { model.GetFeedRequest with SearchQuery = query }
-    { model with GetFeedRequest = getFeedRequest }, Cmd.none
-  | SetOrdering ordering ->
-    let ordering =
-      match ordering with
-      | ordering when ordering = string Ordering.Updated -> Ordering.Updated
-      | ordering when ordering = string Ordering.Oldest -> Ordering.Oldest
-      | ordering when ordering = string Ordering.Newest -> Ordering.Newest
-      | _ -> Ordering.Updated
-
-    let getFeedRequest =
-      { model.GetFeedRequest with
-          Ordering = ordering
-          Page = 1
-      }
-
-    { model with GetFeedRequest = getFeedRequest }, getUserFeedFromServerCmd getFeedRequest
-  | SetSelectedFeed feedValue ->
-    let feedId =
-      match Guid.TryParse(feedValue) with
-      | true, value -> Some value
-      | false, _ -> None
-
-    let getFeedRequest =
-      { model.GetFeedRequest with
-          Feed = feedId
-          Page = 1
-      }
-
-    { model with GetFeedRequest = getFeedRequest }, getUserFeedFromServerCmd getFeedRequest
   | SetPosts posts -> { model with Posts = posts }, Cmd.none
+  | SetSubscribedFeeds feeds -> { model with SubscribedFeeds = feeds }, Cmd.none
+  | AddFeedToContext feed -> { model with SubscribedFeeds = feed :: model.SubscribedFeeds }, getUserFeedCmd model
+  | SetCurrentUser user -> setUser user model
+  | DeleteFeedFromContext feedId -> deleteFeed feedId model
+  | Search -> search model
+  | SetPage page -> setPage page model
+  | SetSearchQuery query -> setSearchQuery query model
+  | SetOrdering ordering -> setOrdering ordering model
+  | SetSelectedFeed feedValue -> setSelectedFeed feedValue model
 
 let dispose _ = ()
 let store, dispatch = Store.makeElmish init update dispose ()
