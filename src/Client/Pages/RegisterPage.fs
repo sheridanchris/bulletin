062f12ecb3e91eca80c489477b01e2b32d869bac
Christian Sheridan <christiansheridan@outlook.com>
2023-03-03 16:46:42 -0600

    Fix placeholder in email input

--- src/Client/Pages/RegisterPage.fs
+++ src/Client/Pages/RegisterPage.fs
@@ -150,7 +150,7 @@ let Component () =
           </div>
           <div>
             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email address</label>
-            <input @keyup={EvVal(SetEmailAddress >> dispatch)} type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="username">
+            <input @keyup={EvVal(SetEmailAddress >> dispatch)} type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="email address">
             {ValidationErrors.renderValidationErrors state.ValidationErrors "Email address" state.CreateAccountRequest.EmailAddress}
           </div>
           <div>
